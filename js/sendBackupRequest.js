f8dfde1664fc419a9e72fb9c123ce70cb00c91e0
felix <flixromer443@gmail.com>
2022-04-04 10:37:51 -0300

    fix descargar archivos backup

--- js/sendBackupRequest.js
+++ js/sendBackupRequest.js
@@ -2,10 +2,15 @@ function sendRequest(){
     let files=['accounts','activities','families','general_billings','partners_activities','partners']
     document.getElementById('modal-bkp').style.display='none'
     document.getElementById('loader').style.display='block'
-    files.forEach(e=>generateFiles(e))
     document.getElementById('modal-bkp').style.display='block'
     document.getElementById('loader').style.display='none'
     document.getElementById('btn-modal').click()
+    setTimeout(() => {generateFiles(files[0])}, 1000);
+    setTimeout(() => {generateFiles(files[1])}, 2000);
+    setTimeout(() => {generateFiles(files[2])}, 3000);
+    setTimeout(() => {generateFiles(files[3])}, 4000);
+    setTimeout(() => {generateFiles(files[4])}, 5000);
+    setTimeout(() => {generateFiles(files[5])}, 6000);
 
 }
 function generateFiles(file){
@@ -20,7 +25,8 @@ function generateFiles(file){
           },
      
         }).then(function(datos){
-            window.open(`./files/${file}.csv`)
+            console.log(datos)
+            location.href=`./files/${file}.csv`
          })    
         
        } catch (error) {
